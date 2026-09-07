ecd69d7526e1c2cbb816b507d2411c022c46ee26
Mridul Srivastava <mridulsrivastava777@gmail.com>
2026-09-08 00:26:58 +0530

    Time: 3 ms (37.51%), Space: 8.8 MB (78.81%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
-    int solve(int i, string &s, vector<int> &dp, vector<int> &prev){
-        if(i == 0) return 1;
-        if(dp[i] != -1) return dp[i];
-
-        long long ans = (2LL * solve(i - 1, s, dp, prev)) % MOD;
-        if(prev[i] != -1) {
-            ans = (ans - solve(prev[i] - 1, s, dp, prev) + MOD) % MOD;
-        }
-        return dp[i] = ans;
-    }
-
     int distinctSubseqII(string s) {
-        int n = s.size();
+        const int MOD = 1e9 + 7;
+        vector<long long> last(26, 0);
 
-        vector<int> dp(n + 1, -1);
-        vector<int> prev(n + 1, -1);
-        vector<int> last(26, -1);
-
-        for(int i = 1; i <= n; i++){
-            int c = s[i - 1] - 'a';
-            prev[i] = last[c];
-            last[c] = i;
+        for (char c : s) {
+            long long sum = 0;
+            for (int i = 0; i < 26; i++) {
+                sum = (sum + last[i]) % MOD;
+            }
+            last[c - 'a'] = (sum + 1) % MOD;
         }
 
-        return (solve(n, s, dp, prev) - 1 + MOD) % MOD;
+        long long ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans = (ans + last[i]) % MOD;
+        }
+
+        return ans;
     }
 };
