2fc7646a29cbd1d04f8607bde21d0819381eebb0
Mridul Srivastava <mridulsrivastava777@gmail.com>
2026-09-14 14:29:04 +0530

    Time: 0 ms (100%), Space: 9.9 MB (47.04%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -4,7 +4,6 @@ public:
         int width=min(rec1[2],rec2[2])-max(rec1[0],rec2[0]);
         int height=min(rec1[3],rec2[3])-max(rec1[1],rec2[1]);
 
-    
         return width>0 && height>0;
     }
 };
