fb8bb9b4b044bc3f7e255f6b9cf05e7038c370e3
Mridul Srivastava <mridulsrivastava777@gmail.com>
2026-09-23 18:03:56 +0530

    Time: 0 ms (100%), Space: 10.3 MB (61.71%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(string s,unordered_set<string>& dict,int i, string& par, vector<string>& all){
+        if(i==s.size()) all.push_back(par);
+        else{
+            for(int j=i;j<s.size();j++){
+                string word=s.substr(i,j-i+1);
+                if(dict.find(word) != dict.end()){
+                    string par2=par;
+                    if(!par.empty()) par2+=" ";
+                    par2+=word;
+                    dfs(s,dict,j+1,par2,all);
+                }
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(),wordDict.end());
+        vector<string> all;
+        string par;
+        int i=0;
+        dfs(s,dict,i,par,all);
+        return all;
+    }
+};
