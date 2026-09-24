c594f30496b0342acc1371a570c84de591ab972c
Mridul Srivastava <mridulsrivastava777@gmail.com>
2026-09-24 09:14:55 +0530

    Time: 0 ms (100%), Space: 31 MB (52.36%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
 
         for(int i=0;i<n;i++){
-            int sum=0;
-            while(nums[i]){
-                int d=nums[i]%10;
+            int sum=0,x=nums[i];
+            while(x){
+                int d=x%10;
                 sum+=d;
-                nums[i]/=10;
+                x/=10;
             }
 
             if(sum==i) return i;
